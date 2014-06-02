54ed0dfdc96aac4a3c9d3ad5a9b6839f7d15d59d
Dave Wright <dave.william.wright@gmail.com>
2014-06-02 20:09:55 +0100

    Comment policing in bac-builder

--- builder/bac_builder.py
+++ builder/bac_builder.py
@@ -2,6 +2,12 @@ from common.pdb_io import *
 import subprocess
 
 def mutate_residue(atomgroup, resname, new_residue_name=''):
+  """
+  Mutates the residue(s) into new_residue_name. The only atoms retained are
+  the backbone and CB. If esname == new_residue_name the residue is left 
+  untouched.
+  """    
+    
   if resname == new_residue_name:
     return atomgroup
 
@@ -33,6 +39,11 @@ def edit_leap_input(specification, filename):
     return
 
 def amber_parameterize(specification):
+    """
+    Edit the Leap input to reflect the input specification dictionary and run 
+    tLeap to create completed PDB and topology files for the complex (both 
+    prior to and after solvation), the receptor and the ligand.
+    """
 
     leap_input = specification['TARGET_DIR'] + '/leap.in'
 

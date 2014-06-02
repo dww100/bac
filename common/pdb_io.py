f3cbfdfc01e90a599be782feb747e63167ac262d
Derek Groen <djgroennl@gmail.com>
2014-06-02 14:40:34 +0100

    Added basic pdb loading and atom selection functions.

--- common/pdb_io.py
+++ common/pdb_io.py
@@ -1,4 +1,5 @@
 import MDAnalysis as mda
+from operator import attrgetter
 
 def load_pdb(filename):
   u = mda.Universe(filename) 
@@ -6,7 +7,25 @@ def load_pdb(filename):
   # and as 'trajectory' input.
   return u
 
-def print_pdb(filename):
+def print_pdb_file(filename):
   u = load_pdb(filename)
+  print_pdb(u)
+
+def print_pdb(u):
   print u.atoms
   print u.trajectory
+
+def select_atoms(u, select_condition):
+  return u.selectAtoms(select_condition)
+
+def sort_atom_selection(selection):
+  return sorted(selection, key=attrgetter('number'))
+
+def merge_atom_selections(selections):
+  assert len(selections) > 1
+  
+  out = selections[0]   
+  for s in selections[1:]:
+    out += s
+
+  return sort_atom_selection(out)  
--- common/test_pdb_io.py
+++ common/test_pdb_io.py
@@ -1,16 +1,29 @@
 # This Test opens a simple pdb file (data/init_pdbs/pr/1mui_wat.pdb)
 # It then attempts to manipulate this file using MDAnalyis.
 
-import MDAnalysis
+from pdb_io import *
+from operator import itemgetter, attrgetter
+from nose.tools import assert_equals
 
 data_dir = "../data"
 
+
 def test_load_pdb_file():
-  from pdb_io import load_pdb
   load_pdb("%s/init_pdbs/pr/1mui_wat.pdb" % data_dir)
   #universe = Universe(topology, trajectory)
   print "PDB file loaded!"
 
 def test_print_pdb_file():
-  from pdb_io import print_pdb
-  print_pdb("%s/init_pdbs/pr/1mui_wat.pdb" % data_dir)
+  print_pdb_file("%s/init_pdbs/pr/1mui_wat.pdb" % data_dir)
+
+def test_pdb_select_and_merge():
+  u = load_pdb("%s/init_pdbs/pr/1mui_wat.pdb" % data_dir)
+  target = select_atoms(u, "segid A")
+  s1 = select_atoms(u, "segid A and resnum 10")
+  s2 = select_atoms(u, "segid A and not resnum 10")
+  s3 = s2 + s1 
+  #if s3 is sorted properly, then the last element should be s2[-1]
+  s4 = sort_atom_selection(s3)
+  assert_equals(s2[-1].number, s4[-1].number)
+  s5 = merge_atom_selections([s2,s1])
+  assert_equals(s2[-1].number, s5[-1].number)

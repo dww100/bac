51243e3a90c97dce5eaf72a6f014e4963005e7fb
Dave Wright <dave.william.wright@gmail.com>
2014-06-03 09:29:55 +0100

    Create common interface file. Start with a 1 to 3 amino acid code dictionary

---
+++ common/interface.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+aa1to3 = {
+'A':'ALA',
+'C':'CYS',
+'D':'ASP',
+'E':'GLU',
+'H':'HIS',
+'I':'ILE',
+'K':'LYS',
+'L':'LEU',
+'M':'MET',
+'N':'ASN',
+'P':'PRO',
+'Q':'GLN',
+'R':'ARG',
+'S':'SER',
+'T':'THR',
+'V':'VAL',
+'W':'TRP',
+'Y':'TYR'
+}
